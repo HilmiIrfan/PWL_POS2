dd5a6e5d61e57f78e1262a3ee4f5b2dec661a0a4
HilmiIrfan <hilminaafiudin@gmail.com>
2024-04-05 23:24:17 +0700

    Jobsheet 7

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,17 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[WelcomeController::class,'index']);
+
+Route::group(['prefix'=>'user'], function(){
+    Route::get('/',[UserController::class,'index']);
+    Route::get('/list',[UserController::class,'list']);
+    Route::get('/create',[UserController::class,'create']);
+    Route::get('/',[UserController::class,'store']);
+    Route::get('/{id}',[UserController::class,'show']);
+    Route::get('/{id}/edit',[UserController::class,'edit']);
+    Route::get('/{id}',[UserController::class,'update']);
+    Route::get('/{id}',[UserController::class,'destroy']);
 });
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::get('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-Route::get('/kategori', [KategoriController::class, 'index']);
