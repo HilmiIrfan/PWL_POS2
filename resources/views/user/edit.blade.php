b38d5fe041ed290a1165703c5d932eb662b588f4
HilmiIrfan <hilminaafiudin@gmail.com>
2024-04-05 23:30:51 +0700

    Jobsheet 7

---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.template')
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class=" card-tools"></div>
+        </div>
+        <div class="card-body">
+            @empty($user)
+                <div class="alert alert-danger alert-dismissible">
+                    <h5><i class="icon fas fa-ban"></i>Kesalahan!</h5>
+                    Data yang Anda cari tidak ditemukan.
+                </div>
+                <a href="{{ url('user') }}" class="btn btn-sm btn-default mt-2">Kembali</a>
+            @else
+                <form method="POST" action="{{ url('/user/'.$user->user_id) }}" class="form-horizontal">
+                    @csrf
+                    {!! method_field('PUT') !!}
+                    <div class=" form-group row">
+                        <label
+                    </div>
+                </form>
+            @endempty
+            <a href="{{ url('user') }}" class="btn btn-sm btn-default mt-2">Kembali</a>
+        </div>
+    </div>
+@endsection
+@push('css')
+@endpush
+@push('js')
+@endpush
